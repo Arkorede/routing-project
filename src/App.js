aa401c01642b422321f994bcd07ee8dfd53e99bc
arkorede <israelashaolu2000@gmail.com>
2022-10-19 23:08:16 -0700

    react-routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Home from './Routes/Home'
+import About from './Routes/About'
+import Contact from './Routes/Contact'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Nav from './Nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Routes/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return <div>
+    <h1 className='bg-dark text-white p-3'>Contact</h1>
+
+    <h2 className='mt-5'>My Name is Israel Ashaolu</h2>
+    <h2 className='mt-5'>My Email address is israelashaolu@2000gmail.com</h2>
+    <h2 className='mt-5'>My phone number is 123456789</h2>
+  </div>
+}
+
+export default Contact;
